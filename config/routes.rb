75fc0e844737390c09f64b445f19d958c57819c0
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-03-15 04:25:52 +0000

    [Add]controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,52 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'orders/show'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/edit'
+    get 'customers/show'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admin do
+    get 'items/index'
+    get 'items/show'
+    get 'items/edit'
+    get 'items/new'
+  end
+  namespace :public do
+    get 'destinations/index'
+    get 'destinations/edit'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/confirm'
+    get 'orders/complete'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'cart_items/index'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/soft_delete'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
