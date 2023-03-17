3bc3f26fa6e06f6f141c2aaff680c0b0996659c5
sasaki-2302 <124408816+sasaki-2302@users.noreply.github.com>
2023-03-17 02:20:47 +0000

    [creat]show

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,12 +1,16 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = Customer.find.params[:id]
   end
 
   def edit
-    @customer = Customer.find8params[:id]
+    @customer = Customer.find.params[:id]
   end
 
   def update
+    @customer = Customer.find.params[:id]
+    @customer.update(customer_params)
+    redirect_to customers_mypage_path
   end
 
   def soft_delete
