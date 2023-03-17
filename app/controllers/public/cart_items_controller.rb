0455bcbc8705f098d0fd032dd3643a821a647b93
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-03-17 13:43:17 +0000

    [Add]cart-item,controller,view

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,10 +1,14 @@
 class Public::CartItemsController < ApplicationController
   def index
-    @customer = Customer.find(params[:customer_id])
-    @cart_items = @customer.cart_items.all
+    # @customer = Customer.find(params[:customer_id])
+    @cart_items = CartItem.page(params[:page])
+    @total = 0
   end
 
   def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to cart_items_path
   end
 
   def destroy
@@ -21,6 +25,16 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.save
+    redirect_to cart_items_path
+  end
+
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:customer_id, :item_id, :amount)
   end
 
 end
