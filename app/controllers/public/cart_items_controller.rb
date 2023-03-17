81143e61bebc0bd44c760e03b563d5a7bc54c2c8
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-03-17 09:20:16 +0000

    [Save]cart-item

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,14 +1,23 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @customer = Customer.find(params[:customer_id])
+    @cart_items = @customer.cart_items.all
   end
 
   def update
   end
 
   def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path
   end
 
   def destroy_all
+    @customer = Customer.find(params[:customer_id])
+    @cart_items = @customer.cart_items.all
+    @cart_items.destroy_all
+    redirect_to cart_items_path
   end
 
   def create
