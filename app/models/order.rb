2ed8929d6a51869f076c23773c2e71957bee223c
YukiIshimuroya <122260585+muro89@users.noreply.github.com>
2023-03-17 05:23:21 +0000

    top_finish

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,9 @@
 class Order < ApplicationRecord
-  enum payment: { credit_card: 0, transfer: 1 }
-  enum order_status: { payment_waiting: 0, payment_confirmation: 1, in_production: 2, preparing_delivery: 3, delivered: 4 }
 
   has_many :order_items
   has_many :items
+
+  enum payment: { credit_card: 0, transfer: 1 }
+  enum order_status: { payment_waiting: 0, payment_confirmation: 1, in_production: 2, preparing_delivery: 3, delivered: 4 }
+
 end
