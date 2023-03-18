338cbc1481838adbed33b5d24a4de687d4f3e66d
sasaki-2302 <124408816+sasaki-2302@users.noreply.github.com>
2023-03-18 13:37:41 +0900

    Revert "kotty[genre]"

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,2 @@
 class Genre < ApplicationRecord
-  has_many :items
-  validates :name,presence: true
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,2 @@
 class Item < ApplicationRecord
-  belongs_to :genre
-
-  validates :genre_id, :name, presence: true
 end
