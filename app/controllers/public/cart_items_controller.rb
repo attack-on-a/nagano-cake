e732ac7bbfc3d58c9d2a45c7699ef91a68bd3d2c
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-03-18 13:40:57 +0000

    [Add]controller-item,cart_item

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,17 +1,39 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @customer = current_customer.id
+    @cart_items = CartItem.page(params[:page])
+    @total = 0
   end
 
   def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to cart_items_path
   end
 
   def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path
   end
 
   def destroy_all
+    current_customer.cart_items.destroy_all
+    redirect_to cart_items_path
   end
 
   def create
+    @cart_item = CartItem.new(cart_item_params)
+    current_customer.cart_items.find_by(id: item_id)
+    @cart_item.save
+    redirect_to cart_items_path
+  end
+
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:customer_id, :item_id, :amount)
   end
 
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,12 @@
 class Public::ItemsController < ApplicationController
   def index
+    @items_count = Item.all.count
+    @items = Item.all.page(params[:page]).per(8)
+    @genres = Genre.all
   end
 
   def show
+    @item = Item.find(params[:id])
+    @genres = Genre.all
   end
 end
