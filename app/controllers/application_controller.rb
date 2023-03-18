3bb8480c47200ff6c85d4217e98412dd589e9ac5
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-03-18 07:47:29 +0000

    [Fix]customer-sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # ログイン後のリダイレクト先
   def after_sign_in_path_for(resource_or_scope)
@@ -14,8 +15,14 @@ class ApplicationController < ActionController::Base
     if resource_or_scope == :admin
       new_admin_session_path
     else
-      root_path
+      new_customer_session_path
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :kana_last_name, :kana_first_name, :post_code, :post_address, :phone_number, :email, :is_deleted])
   end
 
 end
