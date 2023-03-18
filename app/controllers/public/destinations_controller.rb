a9f980395b109d0f74c627644e95020984da2c71
YukiIshimuroya <122260585+muro89@users.noreply.github.com>
2023-03-18 02:41:18 +0000

    destination add

--- app/controllers/public/destinations_controller.rb
+++ app/controllers/public/destinations_controller.rb
@@ -1,17 +1,36 @@
 class Public::DestinationsController < ApplicationController
   def index
+    
+    @destination = Destination.new
+    @destinations = Destination.where(customer_id: current_customer.id)
   end
 
   def edit
+    @destination = Destination.find(params[:id])
   end
 
   def create
+    @destination = Destination.new(destination_params)
+    @destination.customer_id = current_customer.id
+    if @destination.save
+      redirect_to  destination_path
+    else
+      render :index
+    end
   end
 
   def update
+    @destination = Destination.find(params[:id])
+    @destination.update(destination_params)
+    redirect_to destination_path
   end
 
   def destroy
+  end
+
+ private
+  def destination_params
+    params.require(:destination).permit(:post_code, :address, :name)
   end
 
 end
