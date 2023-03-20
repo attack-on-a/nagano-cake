c2361462532420be32a36b1e64e72a703ac31063
YukiIshimuroya <122260585+muro89@users.noreply.github.com>
2023-03-20 06:35:46 +0000

    destination.add

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,6 @@ class Order < ApplicationRecord
   enum payment: { credit_card: 0, transfer: 1 }
   enum order_status: { payment_waiting: 0, payment_confirmation: 1, in_production: 2, preparing_delivery: 3, delivered: 4 }
 
-  has_many :order_items
+  has_many :order_detail
   has_many :items
 end
