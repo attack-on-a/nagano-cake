74b23b93143fd628938a205a0a34d04677403d35
kotoe <k0917.kotty@gmail.com>
2023-03-21 09:03:52 +0000

    [Add]finish-orders-new

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,5 +4,8 @@ class Order < ApplicationRecord
 
   enum payment: { credit_card: 0, transfer: 1 }
   enum order_status: { payment_waiting: 0, payment_confirmation: 1, in_production: 2, preparing_delivery: 3, delivered: 4 }
-
+  
+  validates :post_code
+  validates :address
+  validates :number
 end
