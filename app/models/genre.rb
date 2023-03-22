10ab4d3654f838fe86ca7667eb184bc6aacfb61b
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-03-22 06:00:52 +0000

    [Save]

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,16 @@
 class Genre < ApplicationRecord
   has_many :items
   validates :name,presence: true
+
+  scope :merge_genres, -> (genres){ }
+
+  def self.search_items_for(content)
+
+    if method == 'perfect'
+      genres = Genre.where(name: content)
+    end
+
+    return genres.inject(init = []) {|result, genre| result + genre.items}
+
+  end
 end
